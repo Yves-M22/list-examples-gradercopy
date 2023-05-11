72c6108a57eae4f88c2d322d0f1c79bb65664afb
Yves-M22 <sebastianYSM12@gmail.com>
2023-05-10 17:25:53 -0700

    e

--- grade.sh
+++ grade.sh
@@ -8,6 +8,24 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+    if [[ -f student-submission/ListExamples.java ]]
+    then
+        echo 'Correct File'
+    else
+        echo 'Incorrect File'
+        exit
+    fi
+
+file=student-submission/ListExamples.java
+testFile=TestListExamples.java
+cp -r $file $testFile grading-area/
+
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+javac -cp $CPATH grading-area/TestListExamples.java
+
+if [[]]
+echo "Compile Successful"
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
